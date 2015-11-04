423853ead28bb6eb62c1901c9994d4229261be6f
lomegg <unrelated@ya.ru>
2015-11-04 15:08:43 +0100

    Prototype that I'm unhappy with but got some ideas from

--- js/script.js
+++ js/script.js
@@ -1,14 +1,29 @@
 $(function() {
     console.log( "ready!" );
 
-    $( "#box2" ).toggle( "fade", 1500);
-    setTimeout(function()
-    {
-        $( "#box1" ).toggle( "fade", 1700);
-    }, 800);
-    setTimeout(function()
-    {
-        $( "#logo_wrench" ).toggle( "pulsate" );
-        $( "#logo_gear" ).addClass( "rotated" );
-    }, 2000);
+
+});
+
+$(function() {
+    var $wheel_activated = false;
+    $( ".cards .card" ).draggable({
+        stack: ".cards .stackable",
+        snap: ".hr-invisible",
+        scroll: false,
+        start: function() {
+            if (!$wheel_activated){
+                $( "#box2" ).toggle( "fade", 400);
+                setTimeout(function()
+                {
+                    $( "#box1" ).toggle( "fade", 500);
+                }, 300);
+                setTimeout(function()
+                {
+                    $( "#logo_wrench" ).toggle( "pulsate" );
+                    $( "#logo_gear" ).addClass( "rotated" );
+                }, 1000);
+                $wheel_activated = true;
+            }
+        }
+    });
 });
